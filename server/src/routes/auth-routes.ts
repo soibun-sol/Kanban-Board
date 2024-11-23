0bda27e6d87d2af55c3614a7dfcb3563adc1e447
Keegan Hinton <khinton386@gmail.com>
2024-11-22 21:09:53 -0500

    added user authentication to generate and create jwt

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -5,6 +5,25 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  const { username, password } = req.body; // get user and password 
+
+  const user = await User.findOne({
+    where: { username: username },
+  }); // find user in database
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  } 
+
+  const passwordIsValid = await bcrypt.compare(password, user.password); // compare password
+  if (!passwordIsValid) {
+    return res.status(401).json({ message: 'Invalid Password' });
+  }
+
+  const secretKey = process.env.JWT_SECRET_KEY || ''; // get secret key from .env file
+
+  const token = jwt.sign({ username: user.username }, secretKey, {expiresIn: '1h'}); // create token
+  return res.status(200).json({ token: token });
 };
 
 const router = Router();
