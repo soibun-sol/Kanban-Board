96c461411e690ca1c3f853b2c3bbbe9e7deb1a48
Keegan Hinton <khinton386@gmail.com>
2024-11-22 20:53:50 -0500

    wrote code in main. copied, deleted  and switchiong branches.

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,4 +7,5 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+
 };
