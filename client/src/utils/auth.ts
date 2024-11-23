467209f3091a1f4cc4bca599e6955257c35f40c4
Keegan Hinton <khinton386@gmail.com>
2024-11-22 22:30:55 -0500

    added in code to complete methods for auth

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,28 +3,42 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const token = this.getToken();
+    if (token) {
+      return jwtDecode<JwtPayload>(token);
+    } else {
+      return null;
+    }
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    return !!this.getToken() && !this.isTokenExpired(this.getToken());
   }
   
-  isTokenExpired(token: string) {
+  isTokenExpired(token: string): boolean {
     // TODO: return a value that indicates if the token is expired
+    const decoded: JwtPayload = jwtDecode(token);
+    return decoded.exp ? decoded.exp < Date.now() / 1000 : false;
   }
 
   getToken(): string {
     // TODO: return the token
+    return localStorage.getItem('id_token') || '';
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
+    localStorage.setItem('id_token', idToken);
     // TODO: redirect to the home page
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
+    localStorage.removeItem('id_token');
     // TODO: redirect to the login page
+    window.location.assign('/login');
   }
 }
 
